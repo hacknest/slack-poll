e9b2c239b1254cf6de5a047418b31bb6e9040ce1
James Shephard <james@jshephard.net>
2016-05-28 14:32:14 -0700

    initial express & heroku setup

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.set('port', 80);
+
+app.get('/', function(request, response) {
+  response.send('Hello world!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
