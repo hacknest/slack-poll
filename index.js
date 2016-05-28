c77a804198f8e45fc952aec29e063c0c8d9b57c9
James Shephard <james@jshephard.net>
2016-05-28 14:51:55 -0700

    Added post request handling

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', function(request, response) {
   response.send('Hello world!');
 });
 
+app.post('/', function(req, res) {
+  res.send('Hello world!');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
