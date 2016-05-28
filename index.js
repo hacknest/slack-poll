a2f356bc04a920bcb8c8ee59e55ccdccdf83b08f
James Shephard <james@jshephard.net>
2016-05-28 16:23:23 -0700

    Initialize the SQL database on startup

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var fs = require('fs');
+var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,4 +15,36 @@ app.post('/', function(req, res) {
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
+
+    //Initialize the database
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		if (err) {
+			return console.error('Could not connect to postgres', err);
+		}
+
+		//Check for existence of the poll table
+		client.query('SELECT to_regclass(\'poll\')', function(err, result) {
+			if (!err && result.rows[0].to_regclass) {
+				done();
+				return;
+			}
+
+			//Create tables
+			fs.readFile('./resources/init.sql', 'utf8', function(err, data) {
+				if (err) {
+					done();
+					return console.error('Could not read init.sql', err);
+				}
+
+				client.query(data, function (err, result) {
+					if (err) {
+						return console.log('Unable to initialize SQL database');
+					}
+
+					console.log('Successfully initialized SQL database');
+				});
+				done();
+			});
+		});
+	});
 });
