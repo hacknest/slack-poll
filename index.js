92793c5f34a0de38cfdf6374ac57483424547f37
James Shephard <james@jshephard.net>
2016-05-28 14:36:03 -0700

    Fixed port for heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', 80);
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
   response.send('Hello world!');
