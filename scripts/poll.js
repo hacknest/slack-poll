1675385ed78f6f49314ad142c00094efc4d86d78
Tony Dinh <tdinh@mobify.me>
2016-05-28 16:20:50 -0700

    💀  results skeleton method

--- scripts/poll.js
+++ scripts/poll.js
@@ -12,41 +12,52 @@ var pg = require('../node_modules/pg');
 */
 var query = function(queryObj) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      if(err) {
-        return console.error('error fetching client from pool', err);
-      }
-      var args = queryObj.arg || [];
-      client.query(queryObj.query, args, function(err, result) {
-        //call `done()` to release the client back to the pool
-        done();
-
-        if(err) {
-          return console.error('error running query', err);
+        if (err) {
+            return console.error('error fetching client from pool', err);
         }
-        console.log(result);
-      });
+        var args = queryObj.arg || [];
+        client.query(queryObj.query, args, function(err, result) {
+            // client back to the pool
+            done();
+            if (err) {
+                return console.error('error running query', err);
+            }
+            console.log(result);
+            return result;
+        });
     });
 }
 
 var open = function() {
+    // open poll
+};
+
+var close = function(params) {
+    // tear down DB row
+};
+
+var vote = function(params) {
 
 };
 
-var close = function() {
+var results = function(params) {
+    var query =
+        'SELECT * FROM poll WHERE team_id = ' + params.team_id
+        ' AND channel_id = ' + params.channel_id;
 
-};
+    var queryObj = {
+        query: query
+    };
 
-var vote = function(option_id) {
+    var result = query(queryObj);
 
-};
-
-var show = function() {
-
+    var message = '';
+    return message;
 };
 
 module.exports = {
     open: open,
     close: close,
     vote: vote,
-    show: show
+    results: results
 };
