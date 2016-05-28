3a5fb5930028ae9772451f288925cd529bec077e
Tony Dinh <tdinh@mobify.me>
2016-05-28 15:41:31 -0700

    🔥  wrap query

--- scripts/poll.js
+++ scripts/poll.js
@@ -1,3 +1,30 @@
+var pg = require('../node_modules/pg');
+
+/*
+* queryObj = {
+*   query: String,
+*   arg: Array of escaped variables (optional)
+* }
+*
+*/
+var query = function(queryObj) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      if(err) {
+        return console.error('error fetching client from pool', err);
+      }
+      client.query(queryObj.query, queryObj.arg, function(err, result) {
+        //call `done()` to release the client back to the pool
+        done();
+
+        if(err) {
+          return console.error('error running query', err);
+        }
+        console.log(result.rows[0].number);
+        //output: 1
+      });
+    });
+}
+
 var open = function() {
 
 };
