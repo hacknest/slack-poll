0ae54f0b26e017c72698daa326e2e9619bd5e566
James Shephard <james@jshephard.net>
2016-05-28 19:32:11 -0700

    Fixed up SQL.

--- resources/init.sql
+++ resources/init.sql
@@ -1,23 +1,23 @@
 CREATE TABLE poll(
-	team_id INT,
-	channel_id INT,
+	team_id VARCHAR,
+	channel_id VARCHAR,
 	title VARCHAR NOT NULL,
 	PRIMARY KEY(team_id, channel_id)
 );
 
 CREATE TABLE options(
 	id INT,
-	team_id INT,
-	channel_id INT,
+	team_id VARCHAR,
+	channel_id VARCHAR,
 	option VARCHAR NOT NULL,
 	votes INT DEFAULT 0,
 	PRIMARY KEY(id, team_id, channel_id)
 );
 
 CREATE TABLE votes(
-	team_id INT,
-	channel_id INT,
-	user_id INT,
+	team_id VARCHAR,
+	channel_id VARCHAR,
+	user_id VARCHAR,
 	option_id INT NOT NULL,
 	PRIMARY KEY(team_id, channel_id, user_id)
 );
